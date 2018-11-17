b836f74ebaddab7b7dd37a509021705277079f0b
kkekko02 <31859273+kkekko02@users.noreply.github.com>
2018-11-17 17:37:08 +0200

    Add files via upload

--- java/Login.java
+++ java/Login.java
@@ -1,1 +1,215 @@
+package c.tests.winter2018;
 
+/**
+ * This class implements the login in the application. Firstly, checks the
+ * username and password that the user inserts. If the username and password
+ * are correct - there are in the database, the user logs into the system.
+ *
+ */
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Login extends AppCompatActivity {
+
+    // The connection for the database
+    private static Connection conn = null;
+
+    // The username that the user gives as an input
+    private EditText usernameText;
+    // The password that the user gives as an input
+    private EditText passwordText;
+    // The login button
+    private Button buttonLogin;
+    // The image button for the contact us
+    private ImageButton contactBut;
+    private Button forgot;
+    private Button create;
+
+
+    private Button cancel;
+
+    // The String of EditText
+    private String userName, password;
+
+    // Reference to the inner class checkLogin
+    private CheckLogin checkLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        setupUIviews();
+
+        // Button for the layout Contact Us - Out
+
+
+        // if the credentials are correct, the user logs into the application
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (validate()) {
+                    checkLogin = new CheckLogin();
+                    checkLogin.execute();
+                }
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+
+
+            }
+        });
+       create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, CreateAccount.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+
+
+            }
+        });
+        forgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, ForgotPassword.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+
+
+
+
+            }
+        });
+    }
+
+    // Set up the Edit Texts of the Login Layout
+    private void setupUIviews() {
+        usernameText = (EditText) findViewById(R.id.usename_login);
+        passwordText = (EditText) findViewById(R.id.password_login);
+        buttonLogin = (Button) findViewById(R.id.login_button);
+        cancel=(Button)findViewById(R.id.cancel_button);
+        forgot=(Button)findViewById(R.id.forgot_pass_button);
+       create=(Button)findViewById(R.id.createaccount);
+
+    }
+
+    /*
+     * Checks if the user do not enter the credentials.
+     * It returns true if the credentials are correct, otherwise it returns false;
+     */
+    private Boolean validate() {
+        Boolean result = false;
+
+        userName = usernameText.getText().toString();
+        password = passwordText.getText().toString();
+
+        if (userName.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter all the details...", Toast.LENGTH_SHORT).show();
+        } else {
+            result = true;
+        }
+        return result;
+    }
+
+    /*
+     * Firstly, this class connects with the database. Then it checks if the username
+     * and password, that the user entered, are the same with the record that exists in the database.
+     */
+    private class CheckLogin extends AsyncTask<String, String, String> {
+
+        String message = "Try Again! Invalid Credentials";        // The message for the Toast
+
+
+        /**
+         * This method is happening on the background, it checks the saved credentials if they are
+         * matching with the existing records on the database.
+         * @param params
+         * @return null
+         */
+        protected String doInBackground(String... params) {
+
+            try {
+                // Create the connection with database
+                ConnectDB connection = new ConnectDB();
+                conn = connection.establishConnection();
+
+                // If the connection failed
+                if (conn == null) {
+                    message = "Please check your connection!";
+                    return message;
+                }
+
+
+                // The query for login
+                String query = "SELECT * FROM User WHERE UserID='" + userName + "' AND Password= '" + password + "'";
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(query);
+
+                // if the user exists
+                if (rs.next()) {
+
+
+
+                        message = userName;
+
+                        // creates the session and sets the user's username and password
+                        Account account = Account.getUniqueInstance();
+                        account.setAccountUsername(userName);
+
+                        String iscustomer = null;
+
+                        iscustomer = rs.getString("IsCustomer");
+
+                        // sets the user's email and role
+                        //account.setRole(ismanager);
+                        //account.setEmail(email);
+
+                        // navigates into the home page
+                   message = iscustomer;
+
+                    Intent intent = new Intent(Login.this, MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+
+
+                } else {
+                    message = "Try Again! Invalid Credentials";
+                }
+            } catch (Exception ex) {
+                message = ex.getMessage();
+            }
+
+            return message;
+        }
+
+
+        @Override
+        protected void onPostExecute(String z) {
+            // wright the informing message whether everything was successful or not.
+            Toast.makeText(Login.this, z, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
---
+++ java/MainActivity.java
@@ -0,0 +1,143 @@
+package c.tests.winter2018;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    static HomePage home= new HomePage();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.content_main,home).addToBackStack(null).commit();
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        View headerView = navigationView.getHeaderView(0);
+       // final TextView navUsername = (TextView) headerView.findViewById(R.id.kk);
+        TextView koumpi = (TextView) headerView.findViewById(R.id.edo);
+        koumpi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                //navUsername.setText("df");
+
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        Fragment fragment = null;
+        Activity act=null;
+
+
+
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+
+        if (id == R.id.Login) {
+            act = new Login();
+            fragment=null;
+
+            // Handle the camera action
+        }
+        else if (id == R.id.AboutUs) {
+
+        } else if (id == R.id.Products) {
+
+        } else if (id == R.id.CreatePot) {
+
+        } else if (id == R.id.meeting) {
+            fragment = new Contact();
+
+
+        } else if (id == R.id.map) {
+
+        }
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_main, fragment);
+            ft.commit();
+
+        }
+        else if (act!=null){
+            Intent intent = new Intent(MainActivity.this, Login.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
