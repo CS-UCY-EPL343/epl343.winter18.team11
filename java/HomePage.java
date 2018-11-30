d69548fb01cb342da54fbf719a9c86ed626402da
pgrout01 <31855672+pgrout01@users.noreply.github.com>
2018-11-30 22:18:06 +0200

    Add files via upload

--- java/HomePage.java
+++ java/HomePage.java
@@ -1,7 +1,5 @@
-package c.tests.winter2018;
+package c.tests.Team11;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -18,31 +16,11 @@ import android.view.ViewGroup;
  * in the Attendance table.
  */
 
-import android.graphics.Color;
-import android.os.AsyncTask;
-import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.text.SpannableStringBuilder;
-import android.text.style.RelativeSizeSpan;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Time;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 
 public class HomePage extends Fragment {
+    Account account= Account.getUniqueInstance();
 
     private TextView info;
     public String information="\nThe EMIRA routes began in 1969 when the founder, " +
@@ -67,7 +45,7 @@ public class HomePage extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view= inflater.inflate(R.layout.home_page, container , false);
+        View view= inflater.inflate(c.tests.Team11.R.layout.home_page, container , false);
 
 
 
@@ -78,9 +56,8 @@ public class HomePage extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        info = (TextView) getActivity().findViewById(R.id.info);
+        info = (TextView) getActivity().findViewById(c.tests.Team11.R.id.info);
         info.setText(information);
-
 
 
         getActivity().setTitle("Home");
--- java/StartPage.java
+++ java/StartPage.java
@@ -1,20 +1,20 @@
-package c.tests.winter2018;
+package c.tests.Team11;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+
+/** This is the first page that will appear to the user
+ * when he will open the application.
+ */
 
 public class StartPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.start_page);
+        setContentView(c.tests.Team11.R.layout.start_page);
         new Handler().postDelayed(new Runnable() {
             public void run() {
                 startActivity(new Intent(StartPage.this, MainActivity.class));
