dddbd1e97665223e86374cd84512701f3a06e3af
Stefanos <bluemindset@outlook.com>
2019-01-30 18:38:42 +0200

    MAster Details for account activity

--- Android_Branch/app/src/main/java/com/example/myapplication/Navigation.java
+++ Android_Branch/app/src/main/java/com/example/myapplication/Navigation.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,13 +81,18 @@ public class Navigation extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_account) {
+            /*Lanch account */
+            Intent intent = new Intent(
+                    Account_Activity.this,
+                    Navigation.class);
+            startActivity(intent);
+            finish();
+        } else if (id == R.id.nav_contact) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_time) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_shop) {
 
         } else if (id == R.id.nav_share) {
 
