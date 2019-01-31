ccd505788aa2cdb71d580b128bd68aa93ff69174
Stefanos <bluemindset@outlook.com>
2019-01-31 19:26:34 +0200

    Successufully added two three item . ListView works ok, need to implement per one item activity

--- Android_Branch/app/src/main/java/com/example/myapplication/ItemActivity.java
+++ Android_Branch/app/src/main/java/com/example/myapplication/ItemActivity.java
@@ -1,29 +1,44 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ItemActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item);
-        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        toolbar = (Toolbar)findViewById(R.id.toolBar);
+        toolbar.setTitle(getResources().getString(R.string.shop_name));
+        //Find in the listView
+        listView = (ListView)findViewById(R.id.listView);
+        ArrayAdapter<String> mAdapter = new ArrayAdapter<String>(ItemActivity.this,android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.shop));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            /*Pass to another Activity the name */
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(ItemActivity.this, OneItemActivity.class);
+                intent.putExtra("ItemName",listView.getItemAtPosition(position).toString());
+                startActivity(intent);
             }
         });
+        listView.setAdapter(mAdapter);
+
     }
 
 }
