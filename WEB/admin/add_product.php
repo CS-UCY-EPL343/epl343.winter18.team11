501c389fdb347a71e5efd6817301c45bfcd2f16d
sconst09 <sconst09@cs.ucy.ac.cy>
2019-03-05 18:49:13 +0200

    Add files via upload

--- WEB/admin/add_product.php
+++ WEB/admin/add_product.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
         <head>
+        <title>add_product</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">  
-        <meta charset="utf-8">       
-                <link rel = "stylesheet"
-                  type = "text/css"
-                  href = "add_format.css" />
-                  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-             
-             <?php
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+        <link rel = "stylesheet" type = "text/css" href = "add_format.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+        
+        
+        <?php
           function   el(){
    $link = mysqli_connect("localhost", "root","","emira_pottery");
    if (isset($_POST['submit']))
@@ -46,8 +47,60 @@
 <div class="first">
 <h2>Admin</h2>
 </div>
+<div class="jumbotron" style="margin-bottom:0" >
+    <div class="logo">
+      <h1 >Add Product</h1>
+    </div>
+</div>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="home.php">Home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+    <li class="nav-item">
+      <a class="nav-link" href="products.php" >Products</a>
+    </li>
+      <li class="nav-item dropdown">
+  
+        <a class="nav-link dropdown-toggle dropdown-toggle-split" id="dropdownMenuReference" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-reference="parent">
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="products_ovenware.php">Ovenware Pots</a>
+          <a class="dropdown-item" href="products_decorative.php">Decorative Pots</a>
+          <a class="dropdown-item" href="products_food_drink.php">Food & Drink Pots</a>
+          <a class="dropdown-item" href="products_ecclesiastical.php">Ecclesiastical Pots</a>
+          <a class="dropdown-item" href="products_cyprus.php">Cyprus Souvenirs Pots</a>
+          <a class="dropdown-item" href="products_ancient.php">Ancient Pots</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" >Workshop</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="basket.php" >Basket</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact.php" >Contact</a>
+      </li>
+     
+    </ul>
+    
+    <ul class="navbar-nav  my-2 my-lg-0">
+    <li class="nav-item">
+          <a class="nav-link" onclick="window.location.href='login.php'">Login</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" onclick="window.location.href='register.php'">Sign up</a>
+    </li>
+    </ul>
+  </div>
+</nav>
 <div class="container">
-    <h2>Add Product</h2>
+    
     <div class="frame">
  <form class="form-signin" action="/admin/add_products.php" method="post" name="form" enctype="multipart/form-data">
           <label for="product_name">Product Name</label>
@@ -90,10 +143,7 @@ el();
                         </div>
                     </div>
 
-       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>              
-       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-
+      
 
 </body>
 </html>
