ec0d5818ec8959a77f6c59c2a11cecb205bcad03
sconst09 <sconst09@cs.ucy.ac.cy>
2019-03-28 18:34:28 +0200

    Add files via upload

--- WEB/workshop.php
+++ WEB/workshop.php
@@ -2,12 +2,11 @@
 
     include('functions.php');
     if (!isLoggedIn()) {
-
-     
+      header('location: ../login.php');
     }
     
-    
-?>
+    ?>
+
 
 
 <!DOCTYPE html>
@@ -19,21 +18,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="css/workshop-format.css">
-  <link rel="stylesheet" href="css/bootstrap.css">
-  <link rel="stylesheet" href="css/bootstrap-grid.css">
-  <link rel="stylesheet" href="css/bootstrap-grid.min.css">
-  <link rel="stylesheet" href="css/bootstrap-reboot.min.css">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/bootstrap-reboot.css">
-  <link rel="stylesheet" href="css/mdb.css">
-  <link rel="stylesheet" href="css/mdb.min.css">
-  <link rel="stylesheet" href="css/mdb.lite.css">
-  <link rel="stylesheet" href="css/mdb.lite.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+ 
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
  
   
   
@@ -41,7 +29,42 @@
 
 <body>
 			<?php
+  function send_email(){
+
+    $conn = mysqli_connect("localhost", "root","","emira_pottery");
+
+    $datee=$_POST['date'];
+    $timee=$_POST['time'];
   
+
+    $sql= "SELECT email FROM users WHERE id='{$_SESSION['user']['id']}'";
+    $result=mysqli_query($conn,$sql);
+    $row=mysqli_fetch_array($result);
+    $em=$row['email'];
+   
+    $sql= "SELECT username FROM users WHERE id='{$_SESSION['user']['id']}'";
+    $result=mysqli_query($conn,$sql);
+    $row=mysqli_fetch_array($result);
+    $name=$row['username'];
+
+    $to = 'georgia_kap@hotmail.com';
+    $subject = 'Emira Pottery-Workshop';
+    $message = 'User '.$name.' would like to to book a lesson on: '.$datee. ' at '. $timee;
+    $from = $em;
+  
+  
+  
+ 
+  
+     
+    
+    if(mail($to, $subject, $message, $from)){
+        echo 'Your mail has been sent successfully.';
+    } else{
+        echo 'Unable to send email. Please try again.';
+    }
+  
+  }
 			function myFunction(){
     
       
@@ -81,7 +104,7 @@
 
 
 
-<nav class="navbar navbar-expand-lg navbar-dark " style="background-color: #555555">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" href="home.php">Home</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -94,8 +117,7 @@
     </li>
       <li class="nav-item dropdown">
   
-        <a class="nav-link dropdown-toggle dropdown-toggle-split" id="dropdownMenuReference" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-reference="parent">
-        </a>
+        <a class="nav-link dropdown-toggle dropdown-toggle-split" id="dropdownMenuReference" data-toggle="dropdown"  data-reference="parent"></a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="products_ovenware.php">Ovenware Pots</a>
           <a class="dropdown-item" href="products_decorative.php">Decorative Pots</a>
@@ -106,28 +128,10 @@
         </div>
       </li>
       <li class="nav-item">
-      <?php if (isset($_SESSION['success'])) : ?>
-          <form method="post">
-          <a name="w" value="workshop" class="nav-link" href="workshop.php">Workshop</a>
-          </form>
-      <?php endif ?>
-        <?php if (!isset($_SESSION['success'])) : ?>
-        <form method="post">
-          <a name="w" value="workshop" class="nav-link"  data-toggle="modal" data-target="#modalLoginForm">Workshop</a>
-          </form>
-      <?php endif ?>
-
+        <a class="nav-link" href="workshop.php" >Workshop</a>
       </li>
       <li class="nav-item">
-      <?php if (isset($_SESSION['success'])) : ?>
-          
-          <a class="nav-link" href="basket.php">Basket</a>
-     
-      <?php endif ?>
-        <?php if (!isset($_SESSION['success'])) : ?>
-          <a class="nav-link"  data-toggle="modal" data-target="#modalLoginForm">Basket</a>
-          
-      <?php endif ?>
+        <a class="nav-link" href="basket.php" >Basket</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="contact.php" >Contact</a>
@@ -149,7 +153,7 @@
     <?php endif ?>
     <?php if (!isset($_SESSION['success'])) : ?>
 
-        <a class="nav-link"  data-toggle="modal" data-target="#modalLoginForm">Login</a>
+          <a class="nav-link" onclick="window.location.href='login.php'">Login</a>
     <?php endif ?>
 
 
@@ -158,13 +162,13 @@
 
           <?php if (!isset($_SESSION['success'])) : ?>
 
-          <a class="nav-link" data-toggle="modal" data-target="#modalRegisterForm">Sign up</a>
+        <a class="nav-link" onclick="window.location.href='register.php'">Sign up</a>
         <?php endif ?>
 
     </li>
     </ul>
   </div>
-</nav>
+</nav>  
 
 				  
 			
@@ -186,35 +190,39 @@
 
                         <p class="date">But is open to have a look until 21:00 and Saturday 09:00 – 15:00.</p><br/>
 
-                        <div class="second" style="margin:20px 20px; padding-left: 200px;" >   
+
+
+                        					<div class="second" style="margin:20px 20px; padding-left: 200px;" >   
 
 		
-                          <h4>Please choose the time and date for your lesson:</h4>
-                          <div id="theform">
+                                  <h4>Please choose the time and date for your lesson:</h4>
+                                  <div id="theform">
 
-                          <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
+                                  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
 
-                          Date:(required)<br><input type="date" min="2019-02-20" name="date" /><br/>
-                          <br/><br/>
-                                      
-                          Time:(required)<br><input type="time" name="time"/><br/>
-                          <br/><br/>
-                          <input type="submit" name="submit" value="Submit"/>  
-                            
-                          </div>
-                          </div>
-                          <?php
-                            if (!isLoggedIn()) {
-                              header('location: ../login.php');
-                            }else{
-                          myFunction();
-                            }
-                          ?>
+                                  Date:(required)<br><input type="date" min="2019-02-20" name="date" /><br/>
+                                  <br/><br/>
+                                              
+                                  Time:(required)<br><input type="time" name="time"/><br/>
+                                  <br/><br/>
+                                  <input type="submit" name="submit" value="Submit"/>  
+                                    
+                                  </div>
+                                  </div>
+                                  <?php
+                                    if (!isLoggedIn()) {
+                                      header('location: ../login.php');
+                                    }else{
+                                  myFunction();
+                                    }
+                                  ?>
 
 
-                            <div class="clearfix"></div>
+                                    <div class="clearfix"></div>
 
-                          </form>
+                                  </form>
+
+
                 </div>
                 <div class="col-md-6">
                     <div class="image">
@@ -224,8 +232,15 @@
                       </div>
             </div>
             </div>
-					
-								
+
+
+              <?php 
+
+if(isset($_POST['submit'])){
+      send_email();
+  }  
+  
+?>	
 
 				<footer class="footer">
         <div class="container" >
@@ -290,91 +305,5 @@
                 </div>
 	</div>
 	</div>	
-
-   
-  <!-- MODAL LOGIN --> 
-  <div class="modal fade" id="modalLoginForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
-  aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header text-center">
-        <h4 class="modal-title w-100 font-weight-bold">Login</h4>
-        <button style="padding-left:30%;" type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <form method="POST" >
-      <div class="modal-body mx-3">
-        <div class="md-form mb-5">
-          <i class="fa fa-envelope prefix grey-text"></i>
-          <input type="text" id="defaultForm-email" name="username" class="form-control validate">
-          <label data-error="wrong" data-success="right" for="defaultForm-email">Username </label>
-        </div>
-
-        <div class="md-form mb-4">
-          <i class="fa fa-lock prefix grey-text"></i>
-          <input type="password" id="defaultForm-pass" name="password" class="form-control validate">
-          <label data-error="wrong" data-success="right" for="defaultForm-pass">Password</label>
-        </div>
-
-      </div>
-      <div class="modal-footer d-flex justify-content-center">
-        <button type="submit" class="btn btn-default" name="login_btn">Login</button>
-      </div>
-      <p align="center">
-			Not yet a member? <a data-dismiss="modal" data-toggle="modal" data-target="#modalRegisterForm">Sign up</a>
-		</p>
-      </form>
-    </div>
-  </div>
-</div>
-<!-- Material form login -->
-<!-- MODAL REGISTER --> 
-
-<div class="modal fade" id="modalRegisterForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
-  aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header text-center">
-        <h4 class="modal-title w-100 font-weight-bold">Sign up</h4>
-        <button type="button" style="padding-left:30%;" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <form method="POST">
-      <div class="modal-body mx-3">
-        <div class="md-form mb-5">
-          <i class="fa fa-user prefix grey-text"></i>
-          <input type="text" id="orangeForm-name" name="username" class="form-control validate">
-          <label data-error="wrong" data-success="right" for="orangeForm-name">Name</label>
-        </div>
-        <div class="md-form mb-5">
-          <i class="fa fa-envelope prefix grey-text"></i>
-          <input type="email" id="orangeForm-email" name="email" class="form-control validate">
-          <label data-error="wrong" data-success="right" for="orangeForm-email">Email</label>
-        </div>
-
-        <div class="md-form mb-4">
-          <i class="fa fa-lock prefix grey-text"></i>
-          <input type="password" id="orangeForm-pass" name="password_1" class="form-control validate">
-          <label data-error="wrong" data-success="right" for="orangeForm-pass">Password</label>
-        </div>
-        <div class="md-form mb-4">
-          <i class="fa fa-lock prefix grey-text"></i>
-          <input type="password" id="orangeForm-pass1" name="password_2" class="form-control validate">
-          <label data-error="wrong" data-success="right" for="orangeForm-pass">Confirm Password</label>
-        </div>
-      </div>
-      <div class="modal-footer d-flex justify-content-center">
-        <button type="submit" class="btn btn-deep-orange" name="register_btn">Sign up</button>
-      </div>
-      <p align="center">
-	    	Already a member? <a  data-dismiss="modal" data-toggle="modal" data-target="#modalLoginForm" >Login</a>
-	    </p>
-      </form>
-    </div>
-  </div>
-</div>
-
- 	
+			
 </body>
