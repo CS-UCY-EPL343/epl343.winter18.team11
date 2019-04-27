5c0ac69ea11cb91ab493226ce41a765c79619fd2
gkapat01 <gkapat01@cs.ucy.ac.cy>
2019-04-27 11:22:34 +0300

    Add files via upload

--- WEB/admin/add_product.php
+++ WEB/admin/add_product.php
@@ -58,7 +58,7 @@ if (isset($_GET['logout'])) {
 </div>
 
 <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #555555">
-  <a class="navbar-brand" href="/admin/home.php">Admin Home</a>
+  <a class="navbar-brand" href="home.php">Admin Home</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
